b1ee92ba095cbfd224dbc5e1960773445d19de00
tandduong <tanduong1912@gmail.com>
2020-04-10 20:17:22 -0400

    updated menu button, javascript

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+// Select DOM Items
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-items');
+
+// Set Intital State of Menu
+
+let showMenu = false;
+
+menuButton.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuButton.classList.add('close'); 
+        menu.classList.add('show');
+        menuNav.classList.add('show'); 
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+        
+        // Set Menu State
+        showMenu = true;
+    } else {
+        menuButton.classList.remove('close'); 
+        menu.classList.remove('show');
+        menuNav.classList.remove('show'); 
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove
+            ('show'));
+        
+        // Set Menu State
+        showMenu = false;
+    }
+}
